b48d4092b804aa096f922863d6e37c1584bdab27
Cao Nam <caonam.uit@gmail.com>
2021-12-22 07:36:27 +0700

    deploy app

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/blog-app'));
+app.use(express.static('./dist/mock-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/blog-app/'}),
+    res.sendFile('index.html', {root: 'dist/mock-project/'}),
 );
 
 // Start the app by listening on the default Heroku port
